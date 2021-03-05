a2811371ab690a39fd0925fbf35b35bdb79f7b81
jesusbaz11 <bazanjesus@hotmail.com>
2021-03-04 21:48:09 -0600

    Getting All Books

--- book-api.js
+++ book-api.js
@@ -21,4 +21,8 @@ app.post('/book', (req, res) => {
     res.send('Book is added to the database');
 });
 
+app.get('/books', (req, res) => {
+    res.json(books);
+});
+
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
